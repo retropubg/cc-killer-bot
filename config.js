f33754e62e28eaa75cab5f273b173fbe7ab5ece1
telegrambotss211 <telegrambots79@gmail.com>
2025-02-13 18:45:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     numberOfRegisterInOneTime: 6, // no of registration want to make in one command.
     domains: ["lamoshitt.com", "crazzybabe.com"], // names of domain for the emails. 
     oneTimeMailGen: 10 * 100000,
-    telegramBotToken: "7387279346:AAGdIyOX5qb61Lj_4upbbU8smUSdouMCR94", // bot token of telegram bot. Create a bot using https://t.me/BotFather then generate the token of the bot.
-    adminNamesForTelegramBot: ["aarav_0008","hahaha7726"], // name of the admin/developer who's name will be displayed when any unknown user use the command of the bot in telegram.
-    authorisedUserForTelegramBot: ["aarav_0008","hahaha7726"] // username or id of the user who can use the bot.
+    telegramBotToken: "7634550937:AAHtQNjM0ufpIRBf9n4wL6uh3ML1Pt1wZQI", // bot token of telegram bot. Create a bot using https://t.me/BotFather then generate the token of the bot.
+    adminNamesForTelegramBot: ["Tiger_Bhai_X","hahaha7726"], // name of the admin/developer who's name will be displayed when any unknown user use the command of the bot in telegram.
+    authorisedUserForTelegramBot: ["Tiger_Bhai_X","hahaha7726"] // username or id of the user who can use the bot.
 };
