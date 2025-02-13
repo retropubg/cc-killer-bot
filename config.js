d36a9cb3d79dab418d0c2b011d4c108f149046d6
telegrambotss211 <telegrambots79@gmail.com>
2025-02-14 02:34:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
     domains: ["lamoshitt.com", "crazzybabe.com"], // names of domain for the emails. 
     oneTimeMailGen: 10 * 100000,
     telegramBotToken: "7634550937:AAHtQNjM0ufpIRBf9n4wL6uh3ML1Pt1wZQI", // bot token of telegram bot. Create a bot using https://t.me/BotFather then generate the token of the bot.
-    adminNamesForTelegramBot: ["Tiger_Bhai_X","hahaha7726"], // name of the admin/developer who's name will be displayed when any unknown user use the command of the bot in telegram.
-    authorisedUserForTelegramBot: ["5935444987","hahaha7726"] // username or id of the user who can use the bot.
+    adminNamesForTelegramBot: ["Tiger_Bhai_X","5935444987"], // name of the admin/developer who's name will be displayed when any unknown user use the command of the bot in telegram.
+    authorisedUserForTelegramBot: ["Tiger_Bhai_X","5935444987"] // username or id of the user who can use the bot.
 };
