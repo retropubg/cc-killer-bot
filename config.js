008aea7554ae1d17ed00b66536d54caaf604d20c
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-17 20:36:20 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     numberOfRegisterInOneTime: 6, // no of registration want to make in one command.
     domains: ["lamoshitt.com", "crazzybabe.com"], // names of domain for the emails. 
     oneTimeMailGen: 10 * 100000,
-    telegramBotToken: "7634550937:AAHtQNjM0ufpIRBf9n4wL6uh3ML1Pt1wZQI", // bot token of telegram bot. Create a bot using https://t.me/BotFather then generate the token of the bot.
-    adminNamesForTelegramBot: ["Tiger_Bhai_X","5935444987"], // name of the admin/developer who's name will be displayed when any unknown user use the command of the bot in telegram.
-    authorisedUserForTelegramBot: ["Tiger_Bhai_X","5935444987"] // username or id of the user who can use the bot.
+    telegramBotToken: "7642617516:AAEUXIeFRyu7HqYHDaNZ40Z-nVioc8fCO7U", // bot token of telegram bot. Create a bot using https://t.me/BotFather then generate the token of the bot.
+    adminNamesForTelegramBot: ["eretro_7","6699273462"], // name of the admin/developer who's name will be displayed when any unknown user use the command of the bot in telegram.
+    authorisedUserForTelegramBot: ["eretro_7","6699273462"] // username or id of the user who can use the bot.
 };
